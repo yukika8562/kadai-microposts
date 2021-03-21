ba6bcc2ad063ed33f0e85c1a64564b01de96401c
yukika <nofate8785@gmail.com>
2021-03-21 21:06:23 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -26,7 +28,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    pasams.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
 end
