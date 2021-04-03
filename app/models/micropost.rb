deca9083fdf474fee93b71ce011c4eabb28e4e67
yukika <nofate8785@gmail.com>
2021-04-03 20:36:42 +0000

    add dependent destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,6 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favorite_micropost, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationships, source: :user
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :likes, through: :favorites, source: :micropost
 
   def follow(other_user)
